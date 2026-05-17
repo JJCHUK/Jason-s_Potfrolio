c161b81de224dfe6c7a79facab4ebda95f1aaa5b
JJCHUK <chukwukablessingc@gmail.com>
2026-05-17 16:33:33 +0100

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,3 @@
+function Moreaboutme () {
+alert ("Hello I am Jason this is one of the websites I have created and also I am 9 years old and i am turning 10 this year, My main goal is to create a game and become a developer");
+}
